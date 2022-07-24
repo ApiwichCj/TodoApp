7bf46b7f40490015ef6775ada32bf098c83be5e0
อภิวิชญ์ อยู่ภักดี <76488603+ApiwichCj@users.noreply.github.com>
2022-07-24 17:17:42 +0700

    Update

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-function addTodo(){
+var data = [];
+var html = '';
+$("#btn-add").click(() => {
+  html = '';
+  data.push($("#txt").val())
+  console.log(data);
+  for(var i=0; i<data.length; i++) {
+    html += `
+                    <div id="${i}" class="items">
+                        <p>${data[i]}</p>
+                        <div class="btn-control">
+                            <div onclick="del2do(${i})" class="btn-delete">Delete</div>
+                        </div>
+                    </div>
+                `;
+  }
+  $("#items-control").html(html);
+  $("#txt").val("");
+});
 
-    let input = document.getElementById('inputtodo').value;
-    let newli = document.createElement('li');
-    let node = document.createTextNode(input);
-    newli.className = "list-group-item";
-    newli.appendChild(node);
-    let elist = document.getElementById('todolist');
-
-    
-
-    elist.appendChild(newli);
-    elist.appendChild(elbutton);
+function del2do(id) {
+  data.splice(id, 1);
+  // delete data[id];
+  html = '';
+  for(var i=0; i<data.length; i++) {
+    html += `
+                    <div id="${i}" class="items">
+                        <p>${data[i]}</p>
+                        <div class="btn-control">
+                            <div onclick="del2do(${i})" class="btn-delete">Delete</div>
+                        </div>
+                    </div>
+                `;
+  }
+  $("#items-control").html(html);
 }
 
-function delTodo(){
-    let input = document.getElementById('inputtodo').value;
-    this.parentElement.removeChild(this)
-}
